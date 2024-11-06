7c2af6757d5ca97b83c103d93bee7bcda1e0c92b
ladtopi <ladtopi@users.noreply.github.com>
2024-11-06 15:33:25 +0200

    nicer printing with rich

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,16 +1,52 @@
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.table import Table
+
 from reader import PlayerReader
 from stats import PlayerStats
 
 
-def main():
-    reader = PlayerReader(
-        "https://studies.cs.helsinki.fi/nhlstats/2023-24/players")
-    players = reader.get_players()
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+SEASONS = [
+    "2020-21",
+    "2021-22",
+    "2022-23",
+    "2023-24",
+    "2024-25",
+]
 
-    for player in players:
-        print(player)
+def main():
+    console = Console()
+    prompt = Prompt()
+    console.print("NHL statistics by nationality", style="bold cyan")
+
+    season = prompt.ask("Select season", choices=SEASONS, default=SEASONS[-1])
+
+    while True:
+        nationality = prompt.ask("Select nationality", default="FIN")
+
+        reader = PlayerReader(
+            f"https://studies.cs.helsinki.fi/nhlstats/{season}/players")
+        players = reader.get_players()
+        stats = PlayerStats(reader)
+        players = stats.top_scorers_by_nationality(nationality)
+
+        table = Table(title=f"Top scorers of {nationality} season {season}")
+        table.add_column("Player", style="cyan")
+        table.add_column("Team", style="yellow")
+        table.add_column("Goals", style="green")
+        table.add_column("Assists", style="green")
+        table.add_column("Points", style="magenta")
+
+        for player in players:
+            table.add_row(
+                player.name,
+                player.team,
+                str(player.goals),
+                str(player.assists),
+                str(player.points),
+            )
+
+        console.print(table)
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -6,5 +6,10 @@ class Player:
         self.goals = dict['goals']
         self.assists = dict['assists']
 
+    # property getter for points
+    @property
+    def points(self):
+        return self.goals + self.assists
+
     def __str__(self):
-        return f"{self.name:24} {self.team:5} {self.goals:2d} + {self.assists:2d} = {self.goals + self.assists:3d}"
+        return f"{self.name:24} {self.team:5} {self.goals:2d} + {self.assists:2d} = {self.points:3d}"
