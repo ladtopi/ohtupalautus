b128d30acd67942d87b324f1db31e10264ce5971
ladtopi <ladtopi@users.noreply.github.com>
2024-11-06 12:56:16 +0200

    spec print improvements

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,26 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, authors, license, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.authors = authors
+        self.license = license
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
-    def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+    def _stringify_list(self, l):
+        if len(l) < 1:
+            return "-"
+        s = ""
+        for item in l:
+            s += f"\n- {item}"
+        return s
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license or '-'}"
+            f"\n\nAuthors: {self._stringify_list(self.authors)}"
+            f"\n\nDependencies: {self._stringify_list(self.dependencies)}"
+            f"\n\nDevelopment dependencies: {self._stringify_list(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -18,8 +18,10 @@ class ProjectReader:
 
         name = spec["tool"]["poetry"]["name"]
         desc = spec["tool"]["poetry"]["description"]
+        license = spec["tool"]["poetry"]["license"]
+        authors = spec["tool"]["poetry"]["authors"]
         dependencies = [name for name in spec["tool"]["poetry"]["dependencies"]]
         dev_dependencies = [name for name in spec["tool"]["poetry"]["group"]["dev"]["dependencies"]]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(name, desc, dependencies, dev_dependencies)
+        return Project(name, desc, authors, license, dependencies, dev_dependencies)
