565bca87470f7fb30637888b4ae65f19136efa87
ladtopi <ladtopi@users.noreply.github.com>
2024-11-22 08:24:42 +0200

    refactor tennis

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,55 +1,54 @@
+MAP_SCORE = dict(zip(range(5), ["Love", "Fifteen", "Thirty", "Forty", "Game"]))
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.player_1_name = player1_name
+        self.player_2_name = player2_name
+        self.player_1_points = 0
+        self.player_2_points = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+            self.player_1_points = self.player_1_points + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.player_2_points = self.player_2_points + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if self._even():
+            return self._even_score()
+        if self._win():
+            return self._win_score()
+        if self._advantage():
+            return self._advantage_score()
+        return self._normal_score()
+    
+    def _even(self):
+        return self.player_1_points == self.player_2_points
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
+    def _even_score(self):
+        if self.player_1_points >= 3:
+            return "Deuce"
+        return f"{MAP_SCORE[self.player_1_points]}-All"
 
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+    def _advantage(self):
+        return self.player_1_points >= 4 or self.player_2_points >= 4
+    
+    def _player_ahead(self):
+        if self.player_1_points > self.player_2_points:
+            return self.player_1_name
+        return self.player_2_name
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def _advantage_score(self):
+        return f"Advantage {self._player_ahead()}"
 
-        return score
+    def _win(self):
+        max_pts = max(self.player_1_points, self.player_2_points)
+        diff = abs(self.player_1_points - self.player_2_points)
+        return max_pts >= 4 and diff >= 2
+
+    def _win_score(self):
+        return f"Win for {self._player_ahead()}"
+    
+    def _normal_score(self):
+        return f"{MAP_SCORE[self.player_1_points]}-{MAP_SCORE[self.player_2_points]}"
+
