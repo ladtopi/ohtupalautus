35e10daa865724bfcb4134119fbc77766eefc044
ladtopi <ladtopi@users.noreply.github.com>
2024-11-28 13:41:51 +0200

    ex 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import All, And, HasAtLeast, HasFewerThan, Not, PlaysIn
+from matchers import All, And, HasAtLeast, HasFewerThan, Not, Or, PlaysIn
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -40,6 +40,30 @@ def main():
 
     filtered_with_all = stats.matches(All())
     print(len(filtered_with_all))
+
+    print()
+
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print()
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
     
 if __name__ == "__main__":
     main()
